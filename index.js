25662500d01d97428eb90fd2a1dbd9461a7d2319
arielmarvin@gmail.com <arielmarvin@gmail.com>
2021-04-15 22:10:02 -0400

    cambio

---
+++ index.js
@@ -0,0 +1,44 @@
+let who = ['The dog','My grandma','His turtle','My bird'];
+let action = ['ate','peed','crushed','broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+
+function get_excuse(){
+
+   
+
+
+    numero1=Math.floor(Math.random()*who.length)
+    let palabra1=who[numero1];
+    
+
+
+    numero2=Math.floor(Math.random()*action.length)
+    let palabra2=action[numero2];
+    
+
+
+
+    numero3=Math.floor(Math.random()*what.length)
+    let palabra3=what[numero3];
+    
+
+
+
+    
+    numero4=Math.floor(Math.random()*when.length)
+    let palabra4=when[numero4];
+    
+
+
+    
+    let frase1=palabra1+" "+palabra2+" "+palabra3+" "+palabra4
+
+    console.log(frase1);
+
+
+    document.querySelector(".frase1").innerHTML=frase1;
+
+}
+get_excuse()
+
