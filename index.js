a51dd1411b82f3377e4201757bf946370c2519a6
arielstark1 <arielmarvin@gmail.com>
2021-08-07 02:20:57 +0000

    cambio

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let who = ['The dog','My grandma','His turtle','My bird'];
 let action = ['ate','peed','crushed','broke'];
 let what = ['my homework', 'the keys', 'the car'];
 let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
-
+//cambios
 function get_excuse(){
 
    
